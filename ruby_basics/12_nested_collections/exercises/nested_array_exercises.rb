51e21e674be741d7922b6d60fd4d8c48a5e8e6de
misterbanhman <nathan.i.banh@gmail.com>
2022-11-14 14:38:16 -0700

    Finish Exercise 12 nested_arrays.

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -65,4 +65,12 @@ def find_favorite(array_of_hash_objects)
 
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
+  array_of_hash_objects.each do |hash|  
+    hash.each do |k,v|
+      if k == :is_my_favorite? && v == true
+        return hash
+      end
+    end
+  end
+  return nil
 end
